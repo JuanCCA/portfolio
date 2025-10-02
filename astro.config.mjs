a3547092870c853933042755382bfbc581683ddc
JuanCCA <jcantosagudo@gmail.com>
2025-10-02 20:58:24 +0200

    deploy with dockerfile

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  server: {
-    host: '0.0.0.0',
-    port: process.env.PORT ? Number(process.env.PORT) : 3000,
-  },
 vite: {
   plugins: [
     tailwindcss()
