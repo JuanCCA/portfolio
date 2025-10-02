1f3bc3bdad9c2ddee53ddb004c3e27046f5bac20
JuanCCA <jcantosagudo@gmail.com>
2025-10-02 20:07:41 +0200

    dokku deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,10 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT ? Number(process.env.PORT) : 3000,
+  },
 vite: {
   plugins: [
     tailwindcss()
